374f4511298b93188c23c9ee84262f1740e4ec47
zhuweitung <zhuweitung@foxmail.com>
2022-03-28 17:19:08 +0800

    Update rm_overdate_file.sh

--- rm_overdate_file.sh
+++ rm_overdate_file.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 for hash in $(git verify-pack -v .git/objects/pack/*.idx | grep blob | sort -k 3 -n -r | awk '{print$1}'); do
+    echo -e "get file by hash: ${hash}"
     filename=$(git rev-list --objects --all | grep ${hash} | awk '{print$2}' | grep '^wallpaper.*jpg$')
     if [[ "$filename" != "" ]]; then
         if [[ $(git log --pretty=oneline --branches -- ${filename} | wc -l) -eq 2 ]]; then
             echo -e "will remove ${filename} from this repo."
             git filter-branch --force --index-filter "git rm --cached --ignore-unmatch ${filename}" --prune-empty --tag-name-filter cat -- --all
+        else
+            echo -e "file is not overdate: ${filename}"
         fi
-        
+    else
+        echo -e "file's type is incompatible: ${filename}"
     fi
 done
